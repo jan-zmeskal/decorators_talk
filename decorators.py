f753d61a0d1475249d421bf4e9775fba210cdf94
Jan Zmeskal <janzmeskal@email.cz>
2018-05-18 11:35:05 +0200

    Function theory

--- decorators.py
+++ decorators.py
@@ -8,10 +8,10 @@ from IPython import embed
 Topic: Define Function Decorators with functools.wraps
 
 ⚡ What is decorator?
-A decorator is a function that takes a function object as its argument, 
-and returns a function object, 
-and in the process, makes necessary modifications to the input function, 
-possibly enhancing it. 
+A decorator is a function that takes a function object as its argument,
+and returns a function object,
+and in the process, makes necessary modifications to the input function,
+possibly enhancing it.
 
 The result of the wrapping?
 
@@ -23,22 +23,32 @@ The result of the wrapping?
 ⚡ A little bit of theory about functions:
 - Functions are objects like every other
 - They can be assigned to variables and passed around
-- Functions can accept functions as input and also return them
+- Let's inspect some function: __repr__(), __name__ and __call__()
 """
-
-print('test')
-x = 17
 
 
 def hello():
-    print('hello')
+    """Prints words of wisdom."""
+    print('Hello, world of functions!')
 
-embed()
+
+# embed()
+"""
+- Functions can accept functions as input and also return them
+"""
 
 
-def goodbye():
-    print('goodbye cruel world')
+def caller(func):
+    """Accepts a function and calls it for you, lazy programmer!"""
+    print('I am gonna call function "{}" for you!'.format(func.__name__))
+    func.__call__()
 
+
+def useless_function_factory(name):
+    """Returns a function that prints your name. Don't ask me why.""" 
+    def name_printer():
+        print("All hail to magnificient {}!".format(name))
+    return name_printer
 
 embed()
 
